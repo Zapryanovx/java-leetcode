9f119b8729736033224aef66b069f9ff84459a42
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-06-17 19:14:45 +0300

    add solution to p0594 - Longest Harmonious Subsequence

---
+++ src/hashmap/P0594_LongestHarmoniousSubsequence.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/longest-harmonious-subsequence/
+
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P0594_LongestHarmoniousSubsequence {
+    public int findLHS(int[] nums) {
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            m.put(nums[i], m.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int cnt = 0;
+        for (Map.Entry<Integer, Integer> entry: m.entrySet()) {
+            int left = m.getOrDefault(entry.getKey() - 1, 0);
+            int right = m.getOrDefault(entry.getKey() + 1, 0);
+            if (left == 0 && right == 0) {
+                continue;
+            }
+
+            cnt = Math.max(cnt, Math.max(
+                    entry.getValue() + left,
+                    entry.getValue() + right
+            ));
+        }
+        return cnt;
+    }
+}
