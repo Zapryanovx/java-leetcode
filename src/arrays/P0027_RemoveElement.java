3bbe0514fcd7dbd7021d6dff10b5518cd6d6048d
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-06-17 18:27:38 +0300

    add solution to p0027 - Remove Element

---
+++ src/arrays/P0027_RemoveElement.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class P0027_RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length - 1;
+        int removed = 0;
+
+        while ( left <= right) {
+            if (nums[right] == val) {
+                right--;
+            } else if (nums[left] == val) {
+                nums[left] = nums[left] ^ nums[right];
+                nums[right] = nums[left] ^ nums[right];
+                nums[left] = nums[left] ^ nums[right];
+                right--;
+            } else {
+                left++;
+            }
+
+        }
+        return left;
+    }
+}
