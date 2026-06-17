e5d09965ecfee23d39c9c8f64456a10d8fce8b3b
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-06-17 16:15:51 +0300

    add solution to p0001 - Two Sum

---
+++ src/hashmap/P0001_TwoSum.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/two-sum/description/
+
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (m.containsKey(nums[i])) {
+                return new int[]{m.get(nums[i]), i};
+            }
+            m.put(target - nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+}
