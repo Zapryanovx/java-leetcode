2a2591cd65e8a698bf2c32341a55535e2fc2f4a4
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-06-17 17:54:39 +0300

    add solution to p3898 - Find the Degree of Each Vertex

---
+++ src/arrays/P3898_FindTheDegreeOfEachVertex.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/find-the-degree-of-each-vertex/
+
+package arrays;
+
+class P3898_FindTheDegreeOfEachVertex {
+    public int[] findDegrees(int[][] matrix) {
+        int[] degrees = new int[matrix.length];
+        for (int vertex = 0; vertex < matrix.length; vertex++) {
+            int cnt = 0;
+            for (int edge = 0; edge < matrix[vertex].length; edge++) {
+                if (matrix[vertex][edge] == 1) {
+                    cnt++;
+                }
+            }
+            degrees[vertex] = cnt;
+        }
+        return degrees;
+    }
+}
