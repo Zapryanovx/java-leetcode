c2e94e67e6dc5ef23a92b93f5cd2ded3660a5ba1
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-06-18 15:45:53 +0300

    add solution to p1051 Height Checker

---
+++ arrays/P1051_HeightChecker.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/height-checker/description/
+
+import java.util.Arrays;
+
+public class P1051_HeightChecker {
+    public int heightChecker(int[] heights) {
+        int[] expected = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(expected);
+        int cnt = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i]) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
