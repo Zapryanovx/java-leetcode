96bb0ce2d2bf3e2a12548d600eed8ca3f645a10b
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-06-18 18:22:00 +0300

    add solution to p2706 Buy Two Chocolates

---
+++ arrays/P2706_BuyTwoChocolates.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/buy-two-chocolates/description/
+
+public class P2706_BuyTwoChocolates {
+    public int buyChoco(int[] prices, int money) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int price: prices) {
+            if (price <= min1) {
+                min2 = min1;
+                min1 = price;
+            } else if (price <= min2) {
+                min2 = price;
+            }
+        }
+
+        int leftover = money - min1 - min2;
+        return leftover >= 0 ? leftover : money;
+    }
+}
