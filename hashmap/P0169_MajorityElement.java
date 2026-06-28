017bc34785b8d8f270043a97ab3579b0338bce4b
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-06-28 13:32:00 +0300

    add solution to 169. Majority Element

---
+++ hashmap/P0169_MajorityElement.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/majority-element/description/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P0169_MajorityElement {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> m = new HashMap<>();
+        int majority = nums[0];
+
+        for (int num : nums) {
+            m.put(num, m.getOrDefault(num, 0) + 1);
+            if (m.get(num) > m.get(majority)) {
+                majority = num;
+            }
+        }
+
+        return majority;
+    }
+}
